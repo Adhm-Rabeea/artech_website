2675ac6c46a2ae221b9da49fb98ea290a387407a
adhmrabeea9 <adhmrabeea9@gmail.com>
2025-05-29 06:14:12 +0300

    rename home.html => index.html

--- core/views.py
+++ core/views.py
@@ -5,4 +5,4 @@ from django.http import HttpResponse
 def home(request):
     # return HttpResponse("adhm")
     name = "ARtech adhm rabeea"
-    return render(request, "core/home.html",{"name":name})
+    return render(request, "core/index.html",{"name":name})
